ce33baf77ddb5a8093092ba4842528f5a87fed13
sunze <sunze@sunzedeMacBook-Pro.local>
2015-06-28 16:06:52 +0800

    add two filest

---
+++ swift.text/swift.text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  swift.text
+//
+//  Created by sunze on 15/6/28.
+//  Copyright (c) 2015年 sunze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ swift.text/swift.text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  swift.text
+//
+//  Created by sunze on 15/6/28.
+//  Copyright (c) 2015年 sunze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
